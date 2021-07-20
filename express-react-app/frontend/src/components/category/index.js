7a3dc11239c15c7fa705bac4b7a447d23863da0c
jborja-one <jborja.one@gmail.com>
2021-07-20 17:41:20 -0600

    done with category home components

--- express-react-app/frontend/src/components/category/index.js
+++ express-react-app/frontend/src/components/category/index.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { homeCategory } from '../../store/home-category';
+import './Category.css';
 
 function Category() {
 	const dispatch = useDispatch();
@@ -12,9 +13,19 @@ function Category() {
 		dispatch(homeCategory());
 	}, [dispatch]);
 	return (
-		<div>
+		<div className='component-container'>
 			{category.map((item) => {
-				return <div className='category-component'>{item}</div>;
+				return (
+					<div className='category-component'>
+						<div className='category-img'>
+							<img src='https://christopherhowell.net/images/Sleight-of-Hand-Magician-Dai-Vernon-2.jpg'></img>
+						</div>
+						<div className='component-title'>
+							{item}
+							<a href=''>Discover More</a>
+						</div>
+					</div>
+				);
 			})}
 		</div>
 	);
